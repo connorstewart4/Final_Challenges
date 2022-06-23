8138ba734bcc5df472ba551aafe54458b9090c3a
Connor Stewart <connor.stewart1224@gmail.com>
2022-06-22 21:14:52 -0400

    Files for Challenge 1 and Poco created

---
+++ Challenge_1/src/Menu_Poco/MenuPoco.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+public class MenuPoco : List<MenuPoco>
+{
+    public MenuPoco(int mealNumber, string mealName, string description, string ingredients, decimal price)
+    {
+        MealNumber = mealNumber;
+        MealName = mealName;
+        Description = description;
+        Ingredients = ingredients;
+        Price = price;
+    }
+
+    public int MealNumber { get; set; }
+    public string MealName { get; set; }
+    public string Description { get; set; }
+    public string Ingredients { get; set; }
+    public decimal Price { get; set; }
+
+}
