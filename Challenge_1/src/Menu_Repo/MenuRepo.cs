0c9a785d67ff7cf845741ff58a8b9f4046782c7e
Connor Stewart <connor.stewart1224@gmail.com>
2022-06-23 17:27:51 -0400

    Poco Repo created project 1

--- Challenge_1/src/Menu_Repo/MenuRepo.cs
+++ Challenge_1/src/Menu_Repo/MenuRepo.cs
@@ -4,5 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 public class MenuRepo
 {
+    private List<MenuPoco> MenuList = new List<MenuPoco>();
 
+    public void AddObject(MenuPoco Adding)
+    {
+        MenuList.Add(Adding);
+    }
+
+    public void RemoveObject(int mealNumber)
+    {
+        MenuPoco folderToRemove = new MenuPoco(mealNumber, "", "", "", 0.0m);
+        MenuList.Remove(folderToRemove);
+    }
+
+    public List<MenuPoco> GetMenuPocos()
+    {
+        return MenuList;
+    }
 }
